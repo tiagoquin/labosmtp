618be5110070526af97e6031163ce1677af031fe
Tiago Póvoa <tiago.quin@gmail.com>
2019-04-11 11:00:40 +0200

    Add content-type tho it doesn't seem to work

--- src/main/java/smtp/SmtpClient.java
+++ src/main/java/smtp/SmtpClient.java
@@ -60,30 +60,30 @@ public class SmtpClient implements ISmtpClient {
 
     private boolean contactSMTPServer() throws IOException {
 
-        currentLine = reader.readLine();
+        this.readOneLine();
 
         writer.printf("EHLO itsamemario" + CRLF); // We say Hello to the server.
 
-        currentLine = reader.readLine();
-        System.out.println(currentLine);
-
+        this.readOneLine();
 
         return currentLine.startsWith(REQUEST_OKAY);
     }
 
     private void writeMessage () throws IOException {
 
-        currentLine = reader.readLine();
-        System.out.println(currentLine);
-
+        this.readOneLine();
 
         // From: MAIL FROM
-        writer.write(String.format("MAIL FROM:%s%s", message.getFrom(), CRLF));
+        writer.write(String.format("MAIL FROM: %s%s", message.getFrom(), CRLF));
         writer.flush();
 
+        this.readOneLine();
+
         // To: RCPT TO
-        writer.write(String.format("RCPT TO:%s%s", "Camille", CRLF)); // TODO: 2019-04-10 plusieurs
+        writer.write(String.format("RCPT TO: %s%s", "Camille", CRLF)); // TODO: 2019-04-10 plusieurs
         writer.flush();
+
+        this.readOneLine();
 
         // CC ?
 
@@ -91,36 +91,46 @@ public class SmtpClient implements ISmtpClient {
         writer.write("DATA" + CRLF);
         writer.flush();
 
+        this.readOneLine();
+
+        writer.write(String.format("content-type text/plain; charset=\"utf-8\" %s", CRLF));
+
+
 /*        currentLine = reader.readLine();
         if(!currentLine.startsWith(START_INPUT)) {
             throw new IOException("SMTP Not Happy");
         }*/
 
-        writer.write(String.format("From:%s%s", "Chaton", CRLF));
+        writer.write(String.format("From: %s %s", message.getFrom(), CRLF));
 
-        writer.write(String.format("To:%s%s", "Chaton", CRLF)); // TODO: 2019-04-10 plusieurs
+        writer.write(String.format("To: %s %s", "Testitesto", CRLF)); // TODO: 2019-04-10 plusieurs
 
-        writer.write(String.format("From:%s%s", "Chaton", CRLF));
+        writer.write(String.format("Subject: %s %s", message.getSubject(), CRLF));
 
-        writer.write(String.format("Subject:%s%s", message.getSubject(), CRLF));
-
-        writer.write(String.format("Body:%s%s", message.getBody(), CRLF));
+        writer.write(String.format("Body: %s %s", message.getBody(), CRLF));
         writer.flush();
 
         // Data over
         writer.write(CRLF + '.' + CRLF);
         writer.flush();
 
+        this.readOneLine();
+
+    }
+
+    private void readOneLine () throws IOException {
+        currentLine = reader.readLine();
+        System.out.println(currentLine);
     }
 
 
     public static void main(String ... args) throws IOException {
         SmtpClient smtpClient = new SmtpClient("localhost", 2525);
 
-        String[] to = {"Camille"};
+        String[] to = {"camille@mail.com"};
         String[] cc = {};
 
-        smtpClient.sendMessage(new Message("Rémy", to, cc, "Coucou", "ça va ?"));
+        smtpClient.sendMessage(new Message("remy@mail.com", to, cc, "étoiles, magie Voyances", "ça va ? Moi aussi"));
     }
 
 }
